6810237ed2306a4f75efeb77d55dc100f210a532
suryamukkamula <surya.mukkamula4u@gmail.com>
2023-01-29 07:22:43 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 5;
+  int _counter = 2;
 
   void _incrementCounter() {
     setState(() {
